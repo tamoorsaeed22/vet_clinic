3762bec7324b977c15eace8fc0e691e9d61fd318
Tamoor saeed <tamoorsaeed22@gmail.com>
2023-07-12 16:38:23 +0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -9,24 +9,176 @@ SELECT  date_of_birth FROM animals WHERE  (  name = 'Agumon'  OR name = 'Pikachu
   SELECT * FROM animals WHERE neutered = TRUE;
   SELECT * FROM animals WHERE name != 'Gabumon';
    SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
-   -- Beginning a transaction
-   BEGIN;
-   -- Update species to digimon for animals with name ending in mon
-   UPDATE animals SET species = 'unspecified';
-   -- Verifying that change was made
-   SELECT * FROM animals;
-   -- Roll Back the change
-   ROLLBACK;
-   -- verifying that the species columns went back to the state before the transaction.
-   SELECT * FROM animals; UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon'; 
-   -- Update species to pokemon for animals with no species set
-   UPDATE animals SET species = 'pokemon' WHERE species IS NULL; BEGIN;
-   -- Deleting all records
-   DELETE FROM animals;
-   -- rolling back the transaction.
-   ROLLBACK; DELETE FROM animals WHERE date_of_birth > 'Jan 1, 2022'; SAVEPOINT SP1; UPDATE animals SET weight_kg = weight_kg * -1;ROLLBACK TO SP1;UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
-   -- Commit the transaction
-   COMMIT;
-   -- Verifying that change persists after commit
-   SELECT COUNT(*)FROM animals;
-   SELECT COUNT(*)FROM animals WHERE escape_attempts = 0;SELECT AVG(weight_kg)FROM animals;SELECT MAX(escape_attempts),neutered FROM animals GROUP BY neutered;SELECT MIN(weight_kg)FROM animals;SELECT MAX(weight_kg)FROM animals;SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN 'Jan 01,1990'AND 'Dec 31,2000';
+  -- Beginning a transaction
+BEGIN;
+
+UPDATE
+    animals
+SET
+    species = 'unspecified';
+
+-- Verifying that change was made
+SELECT
+    *
+FROM
+    animals;
+
+-- Roll Back the change
+ROLLBACK;
+
+-- verifying that the species columns went back to the state before the transaction.
+SELECT
+    *
+FROM
+    animals;
+
+UPDATE
+    animals
+SET
+    species = 'digimon'
+WHERE
+    name LIKE '%mon';
+
+UPDATE
+    animals
+SET
+    species = 'pokemon'
+WHERE
+    species IS NULL;
+
+BEGIN;
+
+-- Deleting all records
+DELETE FROM
+    animals;
+
+-- rolling back the transaction.
+ROLLBACK;
+
+DELETE FROM
+    animals
+WHERE
+    date_of_birth > 'Jan 1, 2022';
+
+SAVEPOINT SP1;
+
+UPDATE
+    animals
+SET
+    weight_kg = weight_kg * -1;
+
+ROLLBACK TO SP1;
+
+UPDATE
+    animals
+SET
+    weight_kg = weight_kg * -1
+WHERE
+    weight_kg < 0;
+
+COMMIT;
+
+SELECT
+    COUNT(*)
+FROM
+    animals;
+
+SELECT
+    COUNT(*)
+FROM
+    animals
+WHERE
+    escape_attempts = 0;
+
+SELECT
+    AVG(weight_kg)
+FROM
+    animals;
+
+SELECT
+    MAX(escape_attempts),
+    neutered
+FROM
+    animals
+GROUP BY
+    neutered;
+
+SELECT
+    MIN(weight_kg)
+FROM
+    animals;
+
+SELECT
+    MAX(weight_kg)
+FROM
+    animals;
+
+SELECT
+    AVG(escape_attempts)
+FROM
+    animals
+WHERE
+    date_of_birth BETWEEN 'Jan 01,1990'
+    AND 'Dec 31,2000';
+
+SELECT
+    name
+FROM
+    animals
+    INNER JOIN owners ON animals.owner_id = owners.full_name
+WHERE
+    animals.owner_id = 'Melody Pond';
+
+SELECT
+    animals.name
+FROM
+    animals
+    INNER JOIN species ON animals.species_id = species.name
+WHERE
+    animals.species_id = 'Pokemon';
+
+SELECT
+    full_name,
+    name
+FROM
+    animals
+    RIGHT JOIN owners ON animals.owner_id = owners.full_name;
+
+SELECT
+    animals.name
+FROM
+    animals
+    JOIN owners ON animals.owner_id = owners.full_name
+WHERE
+    animals.species_id = 'Digimon'
+    AND owners.full_name = 'Jennifer Orwell';
+
+SELECT
+    animals.name
+FROM
+    animals
+    JOIN owners ON animals.owner_id = owners.full_name
+WHERE
+    animals.escape_attempts = 0
+    AND owners.full_name = 'Dean Winchester';
+
+SELECT
+    full_name
+FROM
+    animals
+    JOIN owners ON animals.owner_id = owners.full_name
+GROUP BY
+    full_name
+ORDER BY
+    COUNT(animals.owner_id) DESC
+LIMIT
+    1;
+
+SELECT
+    animals.species_id,
+    COUNT(animals.species_id) AS animals
+FROM
+    animals
+    JOIN species ON animals.species_id = species.name
+GROUP BY
+    animals.species_id;
